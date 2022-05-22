72cfb338d9fa335b332b24b69291443e7d1fdb6b
Abraham Kong <abraham.jkong@gmail.com>
2022-05-22 13:21:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 import streamlit as st
 import tensorflow as tf
@@ -7,7 +6,11 @@ from tensorflow.keras.preprocessing.image import img_to_array, load_img, ImageDa
 import numpy as np
 import tensorflow as tf
 import joblib
+from loadclass import Load_model
+from vgg_model import VGG19_model
+import cv2
 
+# Gender Classification
 def image_preprocessing(image):
   input_image = image
   face, confidence = cv.detect_face(input_image)
@@ -17,63 +20,77 @@ def image_preprocessing(image):
   img_array = img_to_array(resize_image)
   final_image = np.expand_dims(img_array, axis=0)
   return final_image
-
 def predict(preprocessed_image):
   my_model = tf.keras.models.load_model('/models/gender_detection.model')
   labels = ["Man","Woman"]
   prediction = my_model.predict(preprocessed_image)[0]
   Predicted_label = labels[np.argmax(prediction)]
   return Predicted_label
-
 def gender_classification(opencv_image):
+    st.subheader("Gender Classificaiton")
     preprocessed_image = image_preprocessing(opencv_image)
     prediction = predict(preprocessed_image)
     return prediction
-
-### Pooja's Code
+# Facemask Detection
 def mask_predict(img):
-    model = joblib.load('/models/part-a-model.sav')
+    st.subheader("Mask Detection")
+    model = joblib.load('/models/gender_detection.model')
     # if type(img) == str:
     #     img = cv2.imread(img)
     img = cv2.resize(img,(200,200))
     img = img / 255
     if model.predict(np.array([img]))[0] > 0.5:
-        predict = 0 # Mask Recognized
-        predictString = "masked"
-    else:
-        predict = 1 # Mask not Recognized
+        predict = 1
         predictString = "unmasked"
-      
-    st.write("Facemask Detection: This person is : ",  predictString)
+    else:
+        predict = 0
+        predictString = "masked"
+    st.write("Facemask Detection: This person is: ",  predictString)
     return predict
-
-
+# Facemask Removal
+def facemaskRemoval(img):
+      mask_g = "/models/MaskG"
+      face_g = "/models/FaceG"
+      face_D_region = "/models/face_D_region"
+      face_D_whole = "/models/face_D_whole"
+      mask_g_model = tf.keras.models.load_model(mask_g, compile=False)
+      face_g_model= tf.keras.models.load_model(face_g, compile=False)
+      face_D_whole_model= tf.keras.models.load_model(face_D_whole, compile=False)
+      face_D_region_model= tf.keras.models.load_model(face_D_region, compile=False)
+      vgg_model = VGG19_model()
+      try:
+        test = Load_model(mask_g_model, face_g_model, mask_checkpoint_dir="/models/mask32_checkpoints", face_checkpoint_dir="/models/face_checkpoints")
+        test.load()
+        # st.image(img,channels="BGR")
+      except:
+        print("some error")
+      img = test.one_predict(img)
 # Define the Image function
 def predictImage(uploaded_file):
-  # picture = Image.open(r img)  
-  # picture = picture.save("dolls.jpg") 
-
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
-
-
     #call pooja's function/model to check if mask exist
     if (mask_predict(opencv_image) == 1):
-      st.write("Gender Classificxation: This person is a : ",  gender_classification(opencv_image))
+      st.write("Gender Classification: This person is a ",  gender_classification(opencv_image))
     else:
-      #run GAN model to remove mask
-      pass
+      facemaskRemoval(opencv_image)
+      removed_mask = cv2.imread('unmasked.jpeg')
       #finally call the gender classificaiton function
-      st.write("Gender Classificxation: This person is a : ",  gender_classification(opencv_image))
-
+      st.write("Gender Classification: This person is a ",  gender_classification(removed_mask))
 # Create the Application
 st.title('Gender Classification, Facemask Detection, and Facemask Removal')
-
 uploaded_file = st.file_uploader("Choose a image file", type="jpeg")
-
-
 input = st.button('Predict')
 # Generate the prediction based on the users input
 if input:
-    st.image(uploaded_file, channels="BGR")
+    st.image(uploaded_file, channels="BGR",width=150)
     predictImage(uploaded_file)
+
+
+
+
+
+
+
+
+
